72999d270f0f455654bfa5a1321bc62f0eeb4c6f
keeganthomp <whereiskeegan@gmail.com>
2017-07-19 14:32:59 -0500

    adding files and structure

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">
+            <h1>Earthquakes!</h1>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
