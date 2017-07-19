cc0702c6d0d4141d3a7fc7876a65eb6adeab6025
keeganthomp <whereiskeegan@gmail.com>
2017-07-19 15:22:22 -0500

    finishing up

--- src/components/EarthquakeInfo.js
+++ src/components/EarthquakeInfo.js
@@ -3,7 +3,12 @@ import "../styles/App.css";
 
 class EarthquakeInfo extends Component {
   render() {
-    return <div className="earthquake-title">sdfasdfasldfkhak</div>;
+    return (
+      <div className="earthquake-title">
+        This is a list of 8 Earthquakes occurring on the morning of July 14th
+        across the United States
+      </div>
+    );
   }
 }
 
